1857a3c6bb8b0ad301d6dddc28095551343aa03a
William Keilsohn <kingwilliam42@gmail.com>
2019-04-07 13:39:54 -0400

    Numbers
    Simple project looking at numeric types

---
+++ numbers/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int num1 = 23;
+
+long num2 = 233333333333333333;
+
+short num3 = 3;
+
+float num4 = 24.67;
+
+int main()
+{
+    cout << num1 << endl;
+    cout << num2 << endl;
+    cout << num3 << endl;
+    cout << num4 << endl;
+    return 0;
+}
