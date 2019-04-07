ed5b8ecf40a51e11ea8a99cf3a74bfff0a556834
William Keilsohn <kingwilliam42@gmail.com>
2019-04-07 13:17:18 -0400

    First Project from the course
    Simple hellow world style project.

---
+++ Project_1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int numberCats = 5;
+
+int main()
+{
+    numberCats += 1;
+    cout << "There are " << numberCats << " cat(s)." << endl;
+    return 0;
+}
