1b967791232b5568c208ec3e7991c1be2f9e9883
William Keilsohn <kingwilliam42@gmail.com>
2019-04-07 13:18:47 -0400

    Intro Classes
    Introduction to Classes in C++
    Example revolves around cats.

---
+++ basic_classes/cat.cpp
@@ -0,0 +1,9 @@
+#include "cat.h"
+
+#include <iostream>
+
+using namespace std;
+
+void Cat::speak() {
+    cout << "Meow" << endl;
+}
---
+++ basic_classes/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "cat.h"
+
+using namespace std;
+
+
+int main() {
+
+    Cat cat1;
+
+    cat1.speak();
+
+    return 0;
+}
