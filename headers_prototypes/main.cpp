43abc4573439883cf17cf4a5caebc0c19592e745
William Keilsohn <kingwilliam42@gmail.com>
2019-04-07 13:35:54 -0400

    Headers and Prototypes
    Project examining how headers and prototypes work in C++

---
+++ headers_prototypes/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// My utilities folder:
+#include "utils.h"
+
+using namespace std;
+
+int main()
+{
+    sayHello();
+    return 0;
+}
+
+
+void sayHello(){
+    cout << "Hello world!" << endl;
+}
